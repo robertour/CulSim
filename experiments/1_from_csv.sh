047c43ad0f2e1f36ef2715f49cf42e1ecc2ba384
tico <roberto.ur@protonmail.ch>
2016-04-27 17:28:39 -0400

    fixing bashes scripts

--- experiments/1_from_csv.sh
+++ experiments/1_from_csv.sh
@@ -2,5 +2,5 @@
 csv_file=$1
 id=$2
 iter=$3
-java -Xss100m -Xms2g -Xmx2g -jar cultural-simulator.jar -ef $csv_file -id $id
-java -Xss100m -Xms2g -Xmx2g -jar cultural-simulator.jar -rd workspace/$id -evs P\(iterations,$iter\) -id $id"_no_catastrophy"
+java -Xss100m -Xms2g -Xmx2g -jar ../executables/culsim.jar -ef $csv_file -id $id
+java -Xss100m -Xms2g -Xmx2g -jar ../executables/culsim.jar -rd workspace/$id -evs P@iterations,$iter -id $id"_no_catastrophy"
--- experiments/3_batch_from_dir.sh
+++ experiments/3_batch_from_dir.sh
@@ -5,5 +5,5 @@ id_param=$3
 preid=$1
 iter=$4
 echo $1 $2 $3 $4
-./2_evs_from_dir.sh workspace/$directory @U,$value $iter $preid "U_"$id_param
-./2_evs_from_dir.sh workspace/$directory @N,0.5,0.5,$value $iter $preid "N_"$id_param 
+./2_evs_from_dir.sh workspace/$directory @U\,$value $iter $preid "U_"$id_param
+./2_evs_from_dir.sh workspace/$directory @N\,0.5\,0.5\,$value $iter $preid "N_"$id_param 
